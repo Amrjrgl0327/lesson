a8390b8dcb0b4a5a16b6e5fc18f3f508d0baa763
Amrjrgl0327 <amrjrgl910327@gmail.com>
2026-08-09 20:31:02 -0800

    react

---
+++ Project/landing/src/app/about/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (<div claasName="w-screen h-screen flex justify-center items-center">
+    about
+  </div>
+  );
+}
---
+++ Project/landing/src/app/page.js
@@ -0,0 +1,14 @@
+
+
+export default function Header() {
+  return (<div>
+    <img src="/Frame 9.png" alt="logo" />
+  <div className="border-solid w-screen h-screen  p-[100px] justify-space between bg-white text-black gap-40">
+    About us Services Use cases Pricing Blog Request a quote
+  </div>
+  </div>
+
+
+
+  );
+}
